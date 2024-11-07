4d4f4dfe950be1c72c646b00ad9f13c7ee3d86bd
Voitsekhovich <leshavoitsekhovich@gmail.com>
2024-11-07 21:24:06 +0300

    4. Navlink, Link, a

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,9 @@ function App() {
       </div>
       <div className={styles.body}>
         <div className={styles.nav}>
-            <div><NavLink to={"/page1"}>Page1</NavLink></div>
-            <div><NavLink to={"/page2"}>Page2</NavLink></div>
-            <div><NavLink to={"/page3"}>Page3</NavLink></div>
+            <div><NavLink className={({isActive})=> isActive?styles.activeNavLink:styles.navLink} to={"/page1"}>Page1</NavLink></div>
+            <div><NavLink to={"/page2"} className={({isActive})=> isActive?styles.activeNavLink:styles.navLink}>Page2</NavLink></div>
+            <div><NavLink className={({isActive})=> isActive?styles.activeNavLink:styles.navLink} to={"/page3"}>Page3</NavLink></div>
           </div>
         <div className={styles.content}>
           <Routes>
