4987d56112404baf77502632123aa39aa61d6681
Voitsekhovich <leshavoitsekhovich@gmail.com>
2024-11-15 15:33:14 +0300

    7. useParams + find

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,55 @@
-import React, { useState } from "react";
-import { Navigate, Route, Router, Routes } from "react-router";
-import { NavLink } from "react-router-dom";
-import styled from "styled-components";
-import { Error404 } from "./components/pages/Error404";
-import { PageOne } from "./components/pages/Adidas";
-import { PageThree } from "./components/pages/Abibas";
-import { PageTwo } from "./components/pages/Puma";
-import { S } from "./components/pages/_styles";
+import React, {useState} from 'react';
 import styles from "./components/Site.module.css";
-import { Model } from "./components/pages/Model";
+import {Adidas} from "./components/pages/Adidas";
+import {Puma} from "./components/pages/Puma";
+import {Abibas} from "./components/pages/Abibas";
+import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
+import {Error404} from "./components/pages/Error404";
+import styled from 'styled-components';
+import {S} from './components/pages/_styles'
+import {Model} from "./components/pages/Model";
 
 
 const PATH = {
-    PAGE1: "/ADIDAS",
-    PAGE2: "/PUMA",
-    PAGE3: "/ABIBAS",
-    EROR404: "/Error404"
-    
-} as const 
+    PAGE1: '/adidas',
+    PAGE2: '/puma',
+    PAGE3: '/abibas',
+} as const
 
 function App() {
-  return (
-    <div>
-      <div className={styles.header}>
-        <h1>HEADER</h1>
-      </div>
-      <div className={styles.body}>
-        <div className={styles.nav}>
-            <S.NavWrapper><NavLink  to={PATH.PAGE1}>ADIDAS</NavLink></S.NavWrapper>
-            <S.NavWrapper><NavLink to={PATH.PAGE2} >PUMA</NavLink></S.NavWrapper>
-            <S.NavWrapper><NavLink  to={PATH.PAGE3}>ABIBAS</NavLink></S.NavWrapper>
-          </div>
-        <div className={styles.content}>
-          <Routes>
-            <Route path="/" element={<Navigate to={PATH.PAGE1}/> }/>
-            <Route path={PATH.PAGE1} element={<PageOne />} />
-            <Route path={PATH.PAGE2} element={<PageTwo />} />
-            <Route path={PATH.PAGE3} element={<PageThree />} />
-            <Route path={PATH.EROR404} element={<Error404 />} />
-            <Route path={"/*"} element={<Error404 />} />
+    return (
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    <S.NavWrapper><NavLink to={PATH.PAGE1}>Adidas</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE2}>Puma</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE3}>Abibas</NavLink></S.NavWrapper>
+                    <a href="page3">page3 HTML</a>
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path={'/'} element={<Navigate to={'/page1'}/>}/>
 
-{/*             <Route path="/*" element={<Navigate to={PATH.EROR404} />} />
- */}            <Route path={'/ADIDAS/:id'} element={<Model/>}/>
-          </Routes>
+                        <Route path={PATH.PAGE1} element={<Adidas/>}/>
+                        <Route path={PATH.PAGE2} element={<Puma/>}/>
+                        <Route path={PATH.PAGE3} element={<Abibas/>}/>
+                        <Route path={'/adidas/:id'} element={<Model/>}/>
+
+                        <Route path={'/*'} element={<Error404/>}/>
+
+                        {/*<Route path={'/page/error'} element={<Error404/>}/>*/}
+                        {/*<Route path={'/*'} element={<Navigate to={'/page/error'}/>}/>*/}
+                    </Routes>
+
+                </div>
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
         </div>
-      </div>
-      <div className={styles.footer}>abibas 2023</div>
-    </div>
-  );
+    );
 }
 
-
 export default App;
+
+
 
--- src/components/pages/Model.tsx
+++ src/components/pages/Model.tsx
@@ -1,23 +1,37 @@
-import { useParams } from "react-router"
-import { adidasArr } from "./Adidas"
+import {adidasArr} from "./Adidas";
+import {useParams} from "react-router-dom";
 
- 
 
-export const  Model =()=>{
-    const params = useParams() 
-    console.log(params )
+export const Model = () => {
+    const params=useParams()
+     const currentModel=adidasArr.find(el=>el.id===Number(params.id))
+
     return (
         <div style={{textAlign:'center'}}>
-            <h2>{adidasArr[Number(params.id)-1].model}</h2>
-            <h4>{adidasArr[Number(params.id)-1].collection}</h4>
-            <h3>{adidasArr[Number(params.id)-1].price}</h3>
-            <img 
-                src={adidasArr[Number(params.id)-1].picture}
-                alt={adidasArr[Number(params.id)-1].model}
-                style={{width:'600px',height:'auto',marginRight:'10px'}}
-            />
+            {currentModel
+                ?<>
+                    <h2>{currentModel.model}</h2>
+                    <h4>{currentModel.collection}</h4>
+                    <h3>{currentModel.price}</h3>
+                    <img
+                        src={currentModel.picture}
+                        alt={currentModel.model}
+                        style={{width: '600px', height: 'auto', marginRight: '10px'}}
+                    />
+                </>
+                :   <h2>Модель отсутствует</h2>
+            }
 
 
+
+        {/*    <h2>{adidasArr[Number(params.id)].model}</h2>*/}
+        {/*    <h4>{adidasArr[Number(params.id)].collection}</h4>*/}
+        {/*    <h3>{adidasArr[Number(params.id)].price}</h3>*/}
+        {/*    <img*/}
+        {/*        src={adidasArr[Number(params.id)].picture}*/}
+        {/*        alt={adidasArr[Number(params.id)].model}*/}
+        {/*        style={{width: '600px', height: 'auto', marginRight: '10px'}}*/}
+        {/*    />*/}
         </div>
-    )
-}
+);
+};
