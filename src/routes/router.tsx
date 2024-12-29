02581bb6acea31c7f04a973239c84d8255dbe1da
Voitsekhovich <leshavoitsekhovich@gmail.com>
2024-12-29 11:54:47 +0300

    UseParams() на синтаксисе 6.4

---
+++ src/routes/router.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    Link,
+} from "react-router-dom";
+import App from "../App";
+import { Abibas } from "../components/pages/Abibas";
+import { Adidas } from "../components/pages/Adidas";
+import { Error404 } from "../components/pages/Error404";
+import { Model } from "../components/pages/Model";
+import { Prices } from "../components/pages/Prices";
+import { Puma } from "../components/pages/Puma";
+
+const PATH = {
+    ADIDAS: '/adidas',
+    PUMA: '/puma',
+    ABIBAS: '/abibas',
+    PRICES: '/prices',
+    MODEL:'/:model/:id' 
+
+
+} as const
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <Error404 />,
+        children: [
+            {
+                path: PATH.ADIDAS,
+                element: <Adidas />
+
+            },
+            {
+                path: PATH.PUMA,
+                element: <Puma />
+
+            },
+            {
+                path: PATH.ABIBAS,
+                element: <Abibas />
+
+            },
+            {
+                path: PATH.PRICES,
+                element: <Prices />
+
+            },
+            {
+                path: PATH.MODEL,
+                element: <Model />
+
+            },]
+    },
+
+]);
+
+{/*<Route path={'/adidas/:id'} element={<Model/>}/>*/}
